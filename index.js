e9f9fa57416bcfc9f0bc3b463dfb8ce9afdb008b
BOT FATHER <94040288+DevBOTFATHER@users.noreply.github.com>
2022-01-24 11:57:58 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const { Discord, Client, Collection, MessageEmbed, Intents } = require('discord.js');
+const client = new Client({ intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES', 'GUILD_INVITES']});
+const config = require('./config.json');
+client.on('ready', () => {
+  console.log(`${client.user.tag} is ready`)
+})
+client.login(config.token)
+//CODE BY BOTFATHER
